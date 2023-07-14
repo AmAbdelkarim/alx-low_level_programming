a2694ddb1e1754c1f746ee9147f0d6296ff67bd7
AmAbdelkarim <phd.abdelkarim@gmail.com>
2023-07-13 19:52:58 -0700

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
-/*
- * main -Entry Point 
- * Return
- */
+/* main -Entry Point*/ 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts(""Programming is like building a multilingual puzzle");
 	return (0);
 }
