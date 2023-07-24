5bfaf9f2429c63c84e59d49d10667d7b5264c5af
AmAbdelkarim <phd.abdelkarim@gmail.com>
2023-07-24 15:41:34 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,13 +6,15 @@
  * @s: string parameter input
  *
  * return: lenght of a string
+ * return _strlen = sucess make return for the next
  */
 
 int _strlen(char *s)
 {
 	int counter;
-	for (counter =0; *s != '\0'; s++)
-	++counter;
+
+	for (counter = 0; *s != '\0'; s++)
+		++counter;
 	return (counter);
 }
 
