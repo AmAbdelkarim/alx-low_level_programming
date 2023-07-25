1711ef271ba1c6f2fd7a82a4bea3d7e6f83ef44d
AmAbdelkarim <phd.abdelkarim@gmail.com>
2023-07-24 19:49:16 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /*
+ * puts2 - Entry point
  * puts2 - prints every character of a string
  *
  * @str: string parameter input
@@ -11,9 +12,10 @@
 void puts2(char *str)
 {
 	int i;
+
 	for (i = 0; str[i] != '\0'; ++i)
 	{
-		if (i % 2 ==0)
+		if (i % 2 == 0)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
