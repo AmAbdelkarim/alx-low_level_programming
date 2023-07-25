b71e20499c120e3aca2dff94472659c2a94ebbdf
AmAbdelkarim <phd.abdelkarim@gmail.com>
2023-07-24 19:39:10 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,12 +5,12 @@
  *
  * @s: pointer to the string to print
  *
- * Return: void 
+ * Return: void
  */
 
 void print_rev(char *s)
 {
-        int i = 0;
+	int i = 0;
 	while (s[i])
 		i++;
 	while (i--)
