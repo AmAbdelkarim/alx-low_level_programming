80513a0b499923f9bfe991851cc2f41182b0b831
AmAbdelkarim <phd.abdelkarim@gmail.com>
2023-07-24 20:03:02 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,7 +2,7 @@
 
 /*
  *
- *
+ * _atoi - Entry Point
  *
  */
 
@@ -10,12 +10,12 @@ int _atoi(char *s)
 {
 	unsigned int num = 0;
 	int sign = 1;
-	
+
 	do {
 		if (*s == '-')
 			sign *= -1;
-		else if (*s >= '0' && *s <='9')
-			num = (num *10) + (*s - '0');
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
 		else if (num > 0)
 			break;
 	} while (*s++);
