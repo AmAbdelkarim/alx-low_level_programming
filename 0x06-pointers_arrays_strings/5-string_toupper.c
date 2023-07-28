24a0d658844faa290ef0055e4e7b65c6b448b228
AmAbdelkarim <phd.abdelkarim@gmail.com>
2023-07-28 14:22:48 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,9 +5,9 @@ char *string_toupper(char *str)
 	int i;
 	for (i = 0; str[i] != '\0', i++)
 	{
-		if (str[i] >= 79 && str[i] <= 122)
+		if (str[i] >= 97 && str[i] <= 122)
 		{
-			str[i] = str[i] -32;
+			str[i] = str[i] - 32;
 		}
 	}
 	return (str);
